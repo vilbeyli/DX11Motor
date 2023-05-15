0d22a7f7269abd4b545a6488f6a80da7d526d249
Volkan Ilbeyli <volkanilbeyli@gmail.com>
2023-05-15 01:53:10 -0700

    move main to own cpp, configure Source/ and Include/ directories

---
+++ Include/TestHeader.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <iostream>
+
+inline void SayHello() { std::cout << "Merhaba!\n"; }
---
+++ Source/Main.cpp
@@ -0,0 +1,7 @@
+#include "TestHeader.h"
+
+int main()
+{
+    SayHello();
+    return EXIT_SUCCESS;
+}
