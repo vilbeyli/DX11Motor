b9ffc43515c6c2176b25d7dff3cdd28d04471d81
Volkan Ilbeyli <volkanilbeyli@gmail.com>
2023-05-15 21:40:41 -0700

    hello window app

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,7 +1,23 @@
-#include "TestHeader.h"
+﻿#include <Windows.h>
 
-int main()
+int WINAPI WinMain(
+    HINSTANCE hInstance,     // Su anki App instance handle'i
+    HINSTANCE hPrevInstance, // Bir onceki App instance handle'i
+    LPSTR lpCmdLine,         // Command Line parametre string'i
+    int nCmdShow             // Uygulamam penceresinin ilk acilista nasil gosterilecegini belirten parametre
+)
 {
-    SayHello();
+    // MessageBoxW(
+    //     _In_opt_ HWND hWnd,
+    //     _In_opt_ LPCWSTR lpText,
+    //     _In_opt_ LPCWSTR lpCaption,
+    //     _In_ UINT uType);
+    MessageBoxW(
+        NULL,               // elimizde henuz bir pencere handle'i olmadigi icin NULL
+        L"Merhaba!",        // Mesaj kutusunda yazacak olan text
+        L"Mesaj Penceresi", // Mesaj penceresinin basligi
+        MB_OK | MB_ICONINFORMATION // Mesaj penceresinde olacak tuslar: "OK", kullanilacak ikon "ICONINFORMATION"
+    );
+
     return EXIT_SUCCESS;
 }
